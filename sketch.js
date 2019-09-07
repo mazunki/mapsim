1b7f9af0a94f7507781843fe423c993e194858fd
mazunki <rolferen@gmail.com>
2019-09-08 00:57:13 +0200

    parametrized colours

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let map;
 let tile_size=20, border_size=1;
 let rows=20, cols=20;
+let green=[90,200,90], purple=[150,50,200];
 
 function setup(){
 	createCanvas(rows*tile_size, cols*tile_size);
-	background(100,200,100);
+	background(green);
 	map = new Array(cols);
 	for (let i=0; i<cols; i++){
 		map[i] = new Array(rows);
@@ -14,9 +15,11 @@ function setup(){
 function draw() {
 	for (var i = 0; i < map.length; i++) {
 		for (var j = 0; j < map[i].length; j++) {
-			map[i][j] = "content";
+			//check data
+
+			// draw tile
 			noStroke();
-			fill(150,50,200);
+			fill(purple);
 			rect(i*tile_size+border_size, j*tile_size+border_size, tile_size-2*border_size, tile_size-2*border_size);
 		}
 	}
