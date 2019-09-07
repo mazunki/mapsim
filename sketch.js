378312b4a380365585c24656f89d3759b83a17cb
mazunki <rolferen@gmail.com>
2019-09-08 01:01:10 +0200

    hardcoded colours added on tile data

--- sketch.js
+++ sketch.js
@@ -8,18 +8,25 @@ function setup(){
 	background(green);
 	map = new Array(cols);
 	for (let i=0; i<cols; i++){
-		map[i] = new Array(rows);
+		map[i] = new Array(rows).fill("standard");
 	}
+	map[5][7] = "poop"
+	map[6][8] = "poop"
 }
 
 function draw() {
 	for (var i = 0; i < map.length; i++) {
 		for (var j = 0; j < map[i].length; j++) {
 			//check data
+			if (map[i][j] == "standard") {
+				color_to_add = green;
+			} else {
+				color_to_add = purple;
+			};
 
 			// draw tile
 			noStroke();
-			fill(purple);
+			fill(color_to_add);
 			rect(i*tile_size+border_size, j*tile_size+border_size, tile_size-2*border_size, tile_size-2*border_size);
 		}
 	}
