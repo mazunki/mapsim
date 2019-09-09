2bb6e67f3daa23b2b1ecbb68aae668f9ecb50fb1
mazunki <rolferen@gmail.com>
2019-09-09 16:24:08 +0200

    added unique class for tile

--- sketch.js
+++ sketch.js
@@ -1,55 +1,36 @@
 let map;
-const tile_size=20, border_size=1;
 const rows=20, cols=20;
-const green=[90,200,90], purple=[150,50,200], gray=[100,100,100], poop=[128,0,0], red=[255,0,0], orange=[255,165,0];
+
 
 function setup(){
-	createCanvas(rows*tile_size, cols*tile_size);
-	background(gray);
+	my_area = createCanvas(rows*tile_size, cols*tile_size);  // define an area for the html
+	my_area.mousePressed(clickListener);
+
+	background(gray); // default background
+	// generate tiles for all positions
 	map = new Array(cols);
 	for (let i=0; i<cols; i++){
-		map[i] = new Array(rows).fill(green);
+		map[i] = new Array(rows);
+		for (let j=0; j<rows; j++)
+			map[i][j] = new Tile(i, j);
 	}
-	//eyes
-	for (var i = 5; i < 10; i++) {
-		map[6][i] = poop;
-		map[13][i] = poop;
-	}
-	//mouth
-	for (var i = 5; i < 15; i++) {
-		map[i][13] = red;
-	}
-	//cute smile
-	map[4][12] = red;
-	map[15][12] = red;
 
 }
 
 function draw() {
 	for (let i = 0; i < map.length; i++) {
 		for (let j = 0; j < map[i].length; j++) {
-			//check data
-			color_to_add = map[i][j];
-
-			// draw tile
-			noStroke();
-			fill(color_to_add);
-			rect(i*tile_size+border_size, j*tile_size+border_size, tile_size-2*border_size, tile_size-2*border_size);
+			if (map[i][j].changed) {
+				map[i][j].draw();
+			}
 		}
 	}
 }
 
-function mouseClicked(){
+function clickListener(){
 	let estimateX, estimateY;
 	index_x = floor(mouseX/tile_size);
 	index_y = floor(mouseY/tile_size);
 
-	map[index_x][index_y] = orange;
-}
-function mouseDragged(){
-	let estimateX, estimateY;
-	index_x = floor(mouseX/tile_size);
-	index_y = floor(mouseY/tile_size);
-
-	map[index_x][index_y] = orange;
+	map[index_x][index_y].newColor(orange);
 }
---
+++ tile.js
@@ -0,0 +1,38 @@
+const tile_size=20, border_size=0;
+const green=[90,200,90], purple=[150,50,200], gray=[100,100,100], poop=[128,0,0], red=[255,0,0], orange=[255,165,0];
+
+
+class Tile {
+	constructor(pos_x, pos_y){
+		this.x = pos_x;
+		this.y = pos_y;
+		// checkers!
+		if ((pos_x+pos_y)%2 == 0){
+			this.color = green;
+		} else {
+			this.color = red;
+		}
+		this.changed = true;
+	}
+
+	newColor(color){
+		this.changed = true;
+		this.color = color;
+		//console.log(this.x + "," + this.y + " new color " + color)
+	}
+
+	mouseClicked(){
+		this.newColor(orange);
+	}
+	mouseDragged(){
+		mouseClicked();
+	}
+
+	draw(){
+		noStroke();
+		fill(this.color);
+		this.rect = rect(this.x*tile_size+border_size, this.y*tile_size+border_size, tile_size-2*border_size, tile_size-2*border_size);
+		this.changed = false;
+	}
+
+}
